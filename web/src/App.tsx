ab7c40d886e2bb3886f684b00024152a814c9324
ZHOURA-24 <zhourateam@gmail.com>
2024-05-29 21:16:25 +0700

    feat:items

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,21 +1,64 @@
-import { CardItem } from "@/components/card-item"
+import { CardItem, ItemProps } from "@/components/card-item"
 import { AppLayout } from "@/components/layouts/app-layout"
+import { useState } from "react"
+import { useSearchParams } from "react-router-dom"
+import { useNuiEvent } from "@/hooks/useNuiEvent"
+import { debugData } from "@/utils/debugData"
 
 function App() {
+  const [items, setItems] = useState<ItemProps[]>([])
+  const [seach] = useSearchParams()
+
+  useNuiEvent("setItems", setItems)
+
   return (
     <AppLayout>
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
-      <CardItem />
+      {items.filter((item) => item.label.toLowerCase().includes(seach.get("q")?.toLowerCase() || '')).map((item) =>
+        <CardItem key={item.name} {...item} />
+      )}
     </AppLayout>
   )
 }
 
 export default App
+
+
+debugData([
+  {
+    action: "setItems",
+    data: [
+      {
+        name: "burger",
+        label: "Burger"
+      },
+      {
+        name: "cocktail",
+        label: "Cocktail"
+      },
+      {
+        name: "lemonade",
+        label: "Lemonade"
+      },
+      {
+        name: "milkteaboba",
+        label: "Milk Tea Boba"
+      },
+      {
+        name: "pasta",
+        label: "Pasta"
+      },
+      {
+        name: "ramen",
+        label: "Ramen"
+      },
+      {
+        name: "sushi",
+        label: "Sushi"
+      },
+      {
+        name: "wine",
+        label: "Wine"
+      },
+    ],
+  },
+])
--- web/src/components/card-item.tsx
+++ web/src/components/card-item.tsx
@@ -1,7 +1,22 @@
-export const CardItem = () => {
+export type ItemProps = {
+    name: string
+    label: string
+}
+
+export const CardItem = ({
+    name,
+    label
+}: ItemProps) => {
     return (
-        <div className="border h-[100px]">
-            CardItem
+        <div className="relative h-[100px] flex flex-col items-center justify-between hover:cursor-pointer">
+            <img
+                src={`./images/${name}.png`}
+                alt={name}
+                className="w-[70px] h-[70px] z-10 bg-repeat bg-center object-contain self-center hover:scale-110 duration-300 bg-contain"
+            />
+            <div className="w-full bg-accent rounded flex justify-center">
+                <span className="overflow-hidden whitespace-nowrap text-ellipsis">{label}</span>
+            </div>
         </div>
     )
 }
